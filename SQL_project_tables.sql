bf6f2b7a3adbb16541ddb44c44de4e9956d449ec
TomVrazina <vrazina.tom2@gmail.com>
2023-02-22 00:48:39 +0100

    Update SQL_project_tables.sql

--- SQL_project_tables.sql
+++ SQL_project_tables.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE t1 AS ( 
+CREATE OR REPLACE TABLE cz_payroll_select_gen AS ( 
 	SELECT
 		AVG(value) AS revenue,
 		industry_branch_code, 
@@ -13,23 +13,23 @@ CREATE OR REPLACE TABLE t1 AS (
 	GROUP BY payroll_year, industry_branch_code, payroll_quarter 
 	ORDER BY industry_branch_code, payroll_year);
 
-CREATE OR REPLACE TABLE t2 AS (
+CREATE OR REPLACE TABLE cz_price_select_gen AS (
 	SELECT AVG(value) AS value_price, category_code, date_from ,YEAR(date_from) AS date_price
 	FROM czechia_price
 	WHERE region_code IS NOT NULL
 	GROUP BY date_from, category_code, date_price);
 
-CREATE OR REPLACE TABLE t4 AS(
+CREATE OR REPLACE TABLE join_price_name AS(
 	SELECT value_price, name AS category_code , date_from, date_price
-	FROM t2
+	FROM cz_price_select_gen
 	JOIN czechia_price_category cpc  
-		ON t2.category_code = cpc.code); 
+		ON cz_price_select_gen.category_code = cpc.code); 
 
-CREATE OR REPLACE TABLE t3 AS
+CREATE OR REPLACE TABLE join_payroll_name AS
 	SELECT revenue, payroll_year, payroll_quarter, name 
-	FROM t1 
+	FROM cz_payroll_select_gen 
 	JOIN czechia_payroll_industry_branch AS cpib 
-		ON t1.industry_branch_code = cpib.code; 
+		ON cz_payroll_select_gen.industry_branch_code = cpib.code; 
 
 	
 -- from countries population a from economy HDP, GINI 	
@@ -47,9 +47,9 @@ CREATE OR REPLACE TABLE eucountry (
 
 CREATE OR REPLACE TABLE t_tomas_vrazina_project_SQL_primary_final AS ( 
 	SELECT *
-	FROM t3
-	LEFT JOIN t4
-		ON t3.payroll_year = t4.date_price);
+	FROM join_payroll_name
+	LEFT JOIN join_price_name
+		ON join_payroll_name.payroll_year = join_price_name.date_price);
 
 CREATE OR REPLACE TABLE t_tomas_vrazina_project_SQL_secondary_final
 WITH cz AS (	
