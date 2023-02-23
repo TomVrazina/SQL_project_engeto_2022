dd259a206e828f7a14620c6e9917cd0efc372d08
TomVrazina <vrazina.tom2@gmail.com>
2023-02-23 13:39:06 +0100

    Update SQL_project_ukol1.sql
    Adding partition by and changing names of common table expression

--- SQL_project_ukol1.sql
+++ SQL_project_ukol1.sql
@@ -2,8 +2,8 @@
 
 -- SHOWS BRANCHES IN WHICH REVENUE DECREASE
 
-WITH cz_payroll2 AS (
-	WITH cz_payroll1 AS (
+WITH for_final_eval AS (
+	WITH for_revenue_growth AS (
 		WITH cz_payroll AS (
 				SELECT revenue, name, payroll_year, payroll_quarter
 				FROM t_tomas_vrazina_project_SQL_primary_final
@@ -12,16 +12,21 @@ WITH cz_payroll2 AS (
 			SELECT AVG(revenue) AS revenue1, name, payroll_year
 			FROM cz_payroll
 			GROUP BY name, payroll_year
-		)
+	)
 	SELECT 
 		revenue1,
 		payroll_year,
-		revenue1 - LAG(revenue1)  OVER (ORDER BY name ,payroll_year ASC) AS revenue_growth,
-		ROUND (-100 + 100* revenue1 / LAG(revenue1) OVER (ORDER BY name ,payroll_year ASC),2) AS revenue_growth_proc,
+		revenue1 - LAG(revenue1)  OVER (
+			PARTITION BY name
+			ORDER BY name ,payroll_year ASC) AS revenue_growth,
+		ROUND (-100 + 100* revenue1 / LAG(revenue1) OVER (
+			PARTITION BY name
+			ORDER BY name ,payroll_year ASC),2) AS revenue_growth_proc,
 		name
-	FROM cz_payroll1
+	FROM for_revenue_growth
 )
 SELECT DISTINCT(name) 
-FROM cz_payroll2
+FROM for_final_eval
 WHERE payroll_year != 2000
-AND revenue_growth < 0
+	AND revenue_growth IS NOT NULL
+	AND revenue_growth < 0;
