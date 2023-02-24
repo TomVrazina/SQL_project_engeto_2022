50221ea5944f1b2d19b98ba97c7ad0a91461699e
TomVrazina <vrazina.tom2@gmail.com>
2023-02-24 20:13:53 +0100

    Update SQL_project_ukol3.sql
    Changing the names of CTE and adding partition by command

--- SQL_project_ukol3.sql
+++ SQL_project_ukol3.sql
@@ -4,43 +4,44 @@ CREATE OR REPLACE TABLE sel_cz_price AS(
 	WHERE value_price IS NOT NULL
 	GROUP BY value, category_code, date_from);
 
+-- Task 3 
 
--- CLEAN CODE 
 
-WITH one AS (
-	WITH two AS (
-		WITH diff AS (
-			WITH regavg AS (
+WITH task_result AS (
+	WITH growth_val_assesment AS (
+		WITH averaging_per_year AS (
+			WITH baseline AS (
 				SELECT value AS value_avg, date_from, category_code 
 				FROM sel_cz_price
 				GROUP BY date_from, category_code
 			)
 			SELECT ROUND(AVG(value_avg),3) val_avg, YEAR(date_from) AS year_payroll, category_code 
-			FROM regavg
+			FROM baseline
 			GROUP BY  year_payroll, category_code 
 		)
 		SELECT 
 			val_avg,
-			ROUND(val_avg / LAG(val_avg) OVER (ORDER BY category_code ,year_payroll ASC),5)*100 - 100 AS value_growth,
+			ROUND(val_avg / LAG(val_avg) OVER (
+				PARTITION BY category_code
+				ORDER BY category_code ,year_payroll ASC),5)*100 - 100 AS value_growth,
 			year_payroll,
 			category_code
-		FROM diff
+		FROM averaging_per_year
 		)
 	SELECT 
 		value_growth,
 		year_payroll,
 		category_code
-	FROM two
-	WHERE year_payroll != 2006
+	FROM growth_val_assesment
+	WHERE value_growth IS NOT NULL
 		AND value_growth > 0
 )
 SELECT 
 	value_growth,
 	year_payroll,
 	category_code
-FROM one 
+FROM task_result 
 ORDER BY value_growth ASC
 LIMIT 1
  
-
 
